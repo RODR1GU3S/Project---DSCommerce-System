501e4e518d06b48279cf34835bc93006d459c6fc
RODR1GU3S <ronaldo.r.silva.78@gmail.com>
2025-11-14 17:28:24 -0300

    Exception handling

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -22,22 +22,32 @@ public class ProductController {
     @GetMapping(value = "/{id}")
     public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
         ProductDTO dto = service.findById(id);
-        return ResponseEntity.ok(dto); // 200 OK with body
+        return ResponseEntity.ok(dto);
     }
 
-    // Esse Método faz uma busca páginada
     @GetMapping
     public ResponseEntity<Page<ProductDTO >> findAll(Pageable pageable) {
         Page<ProductDTO> dto = service.findAll(pageable);
-        return ResponseEntity.ok(dto); // 200 OK with paged body
+        return ResponseEntity.ok(dto);
     }
 
-    // Esse Método Permite a Inserção de Produtos a partir do ex. FrontEnd
     @PostMapping
     public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto) {
         dto = service.insert(dto);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
         return ResponseEntity.created(uri).body(dto); // 201 Created with location header
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto) {
+        dto = service.update(id, dto);
+        return ResponseEntity.ok(dto); // 200 OK with body
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -3,10 +3,15 @@ package com.devsuperior.dscommerce.services;
 import com.devsuperior.dscommerce.dto.ProductDTO;
 import com.devsuperior.dscommerce.entities.Product;
 import com.devsuperior.dscommerce.repositories.ProductRepository;
+import com.devsuperior.dscommerce.services.exceptions.DatabaseException;
+import com.devsuperior.dscommerce.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -19,9 +24,61 @@ public class ProductService {
 
     @Transactional(readOnly = true)
     public ProductDTO findById(Long id) {
-        Product product = repository.findById(id).get();
+        Product product = repository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Recurso não encontrado"));
         return new ProductDTO(product);
     }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO > findAll(Pageable pageable) {
+        Page<Product > result = repository.findAll(pageable);
+        return result.map(x -> new ProductDTO(x));
+    }
+
+    @Transactional
+    public ProductDTO insert(ProductDTO dto) {
+        Product entity = new Product();
+        copyDtoToEntity(dto, entity);
+        entity = repository.save(entity);
+        return new ProductDTO(entity);
+    }
+
+    @Transactional
+    public ProductDTO update(Long id, ProductDTO dto) {
+        try {
+            Product entity = repository.getReferenceById(id);
+            copyDtoToEntity(dto, entity);
+            entity = repository.save(entity);
+            return new ProductDTO(entity);
+        }
+        catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Recurso não encontrado");
+        }
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public void delete(Long id) {
+        if (!repository.existsById(id)) {
+            throw new ResourceNotFoundException("Recurso não encontrado");
+        }
+        try {
+            repository.deleteById(id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Falha de integridade referencial");
+        }
+    }
+
+    private void copyDtoToEntity(ProductDTO dto, Product entity) {
+        entity.setName(dto.getName());
+        entity.setDescription(dto.getDescription());
+        entity.setPrice(dto.getPrice());
+        entity.setImUrl(dto.getImgUrl());
+    }
+}
+
+
+
 
     /*
     //Esse Método busca todos os Serviços/Registros
@@ -31,28 +88,3 @@ public class ProductService {
         return result.stream().map(x -> new ProductDTO(x)).toList();
     }
     */
-
-
-    // Esse Método faz uma busca páginada
-    @Transactional(readOnly = true)
-    public Page<ProductDTO > findAll(Pageable pageable) {
-        Page<Product > result = repository.findAll(pageable);
-        return result.map(x -> new ProductDTO(x));
-    }
-
-    // Esse Método Permite a Inserção de Produtos a partir do FrontEnd
-    @Transactional
-    public ProductDTO insert(ProductDTO dto) {
-
-        Product entity = new Product();
-        entity.setName(dto.getName());
-        entity.setDescription(dto.getDescription());
-        entity.setPrice(dto.getPrice());
-        entity.setImUrl(dto.getImgUrl());
-
-        entity = repository.save(entity);
-
-        return new ProductDTO(entity);
-    }
-
-}
